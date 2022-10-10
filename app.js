553ef471cf97e6972e6af624d67100b6d7529b12
RHamudi <ramonramos.silva16@gmail.com>
2022-10-10 12:31:04 -0300

    Criando conexão com o banco de dados e adicionando template

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
 var express = require("express");
 const app = express();
 const { engine } = require("express-handlebars");
-const Sequelize = require("sequelize");
+const bodyParser = require("body-parser");
+const Post = require("./models/Post");
 
 // Config
 // Template Engine
 app.engine("handlebars", engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.set("views", "./views");
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // Conexão com o banco de dados
-const sequelize = new Sequelize("teste", "root", "ramonramos9137", {
-  host: "localhost",
-  dialect: "mysql",
-});
+
 // Rotas
+
+app.get("/", (req, res) => {
+  Post.findAll().then((posts) => {
+    res.render("home", {
+      Posts: posts,
+    });
+  });
+});
 
 app.get("/cad", (req, res) => {
   res.render("formulario");
+});
+
+app.post("/add", (req, res) => {
+  Post.create({
+    titulo: req.body.titulo,
+    conteudo: req.body.conteudo,
+  })
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((error) => {
+      res.send("Houve um erro" + error);
+    });
 });
 
 app.listen(8080, () => {
