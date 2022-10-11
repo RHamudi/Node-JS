aab0558d96039526a7f3ee5d7aa0be3d8cfbeff7
RHamudi <ramonramos.silva16@gmail.com>
2022-10-11 19:07:01 -0300

    criando conexão mongodb

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require("express");
 const { engine } = require("express-handlebars");
 const bodyParser = require("body-parser");
 const app = express();
+const admin = require("./routes/admin");
+const path = require("path");
+const { default: mongoose } = require("mongoose");
 // const mongoose = require("mongoose");
 
 // Configurações
@@ -14,10 +17,26 @@ app.engine("handlebars", engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.set("views", "./views");
 // mongoose
-
+mongoose.Promise = global.Promise;
+mongoose
+  .connect("mongodb://localhost/blogapp")
+  .then(() => {
+    console.log("conectado ao mongo");
+  })
+  .catch((err) => {
+    console.log("Erro ao conectar-se ao mongo " + err);
+  });
+//public
+app.use(express.static(path.join(__dirname, "public")));
 // Rotas
+app.get("/", (req, res) => {
+  res.send("pagina principal");
+});
 
+app.use("/admin", admin);
 // Outros
+
+// Porta da api
 const port = 8081;
 app.listen(port, () => {
   console.log("Servidor rodando!");
