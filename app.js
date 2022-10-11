f35fc65922471cb4bb55fe6864245b109cc393ec
RHamudi <ramonramos.silva16@gmail.com>
2022-10-11 13:00:00 -0300

    config handlebars/template - bodyParser

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 // Carregando modulos
 const express = require("express");
-const handlebars = require("express-handlebars");
+const { engine } = require("express-handlebars");
 const bodyParser = require("body-parser");
 const app = express();
 // const mongoose = require("mongoose");
 
 // Configurações
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Handlebars
+app.engine("handlebars", engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+// mongoose
 
 // Rotas
 
