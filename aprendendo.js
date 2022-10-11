fa502eeafd43027094f9e43781c9562d014b5573
RHamudi <ramonramos.silva16@gmail.com>
2022-10-11 10:33:51 -0300

    Configurando para evitar erro/mongoose

--- aprendendo.js
+++ aprendendo.js
@@ -1,3 +1,13 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/aprendendo");
+mongoose.Promise = global.Promise;
+mongoose
+  .connect("mongodb://localhost/aprendendo", {
+    useMongoClient: true,
+  })
+  .then(() => {
+    console.log("MongoDB conectado!");
+  })
+  .catch((erro) => {
+    console.log("Houve um ero ao se conectar ao mongodb" + erro);
+  });
