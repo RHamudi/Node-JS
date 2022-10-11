f37cd4513365cb3094db0f86c62ffadd6db746aa
RHamudi <ramonramos.silva16@gmail.com>
2022-10-11 10:51:27 -0300

    Criando model/Criando collection/ iserindo dados na collection

--- aprendendo.js
+++ aprendendo.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+// conectando com o mongodb
 mongoose.Promise = global.Promise;
 mongoose
   .connect("mongodb://localhost/aprendendo", {
@@ -10,4 +11,48 @@ mongoose
   })
   .catch((erro) => {
     console.log("Houve um ero ao se conectar ao mongodb" + erro);
+  });
+// Model usuarios
+
+// Definindo o model
+const UserSchema = mongoose.Schema({
+  nome: {
+    type: String,
+    require: true,
+  },
+  sobrenome: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  idade: {
+    type: Number,
+    require: true,
+  },
+  pais: {
+    type: String,
+  },
+});
+
+// Informando a collection
+mongoose.model("user", UserSchema);
+
+// Inserindo dados na collection
+
+new UserSchema({
+  nome: "Ramon",
+  sobrenome: "Ramons",
+  email: "example@example.com",
+  idade: 19,
+  pais: "Brasil",
+})
+  .save()
+  .then(() => {
+    console.log("user created successfully");
+  })
+  .catch((erro) => {
+    console.log("error creating user " + erro);
   });
