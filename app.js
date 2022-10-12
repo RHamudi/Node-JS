a6d45d2ca2bb93977e3dc59048cd454813a52872
RHamudi <ramonramos.silva16@gmail.com>
2022-10-11 23:05:01 -0300

    Configurando session e flash

--- app.js
+++ app.js
@@ -6,9 +6,26 @@ const app = express();
 const admin = require("./routes/admin");
 const path = require("path");
 const { default: mongoose } = require("mongoose");
+const session = require("express-session");
+const flash = require("connect-flash");
 // const mongoose = require("mongoose");
 
 // Configurações
+//sessão
+app.use(
+  session({
+    secret: "cursonode",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
+// Middleware
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("succses_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  next();
+});
 // Body Parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -28,6 +45,7 @@ mongoose
   });
 //public
 app.use(express.static(path.join(__dirname, "public")));
+
 // Rotas
 app.get("/", (req, res) => {
   res.send("pagina principal");
