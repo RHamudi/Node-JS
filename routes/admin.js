e64e7cc114004766c31227b19fbb772fbd8b9687
Ramon Ramos <94305379+RHamudi@users.noreply.github.com>
2022-10-13 08:56:21 -0300

    categorias and postagens (#2)
    * Node com mysql e sequelize
    
    * .
    
    * first commit
    
    * Instalando o mongoose
    
    * Fazendo conexão com o mongodb/mongoose
    
    * Configurando para evitar erro/mongoose
    
    * Criando model/Criando collection/ iserindo dados na collection
    
    * fix: erro pushing newuser
    
    * .
    
    * first commit
    
    * first commit/install dependencies
    
    * skeleton project
    
    * config handlebars/template - bodyParser
    
    * Adding bootstrap
    
    * criando conexão mongodb
    
    * create: modelo postagem categoria
    
    * routes: rotas admin
    
    * templates
    
    * Configurando session e flash
    
    * criando mensagem de erro utilizando o flash/fazendo validação de erro
    
    * edit categorias/list categorias/ delete categorias
    
    * criando schema postagem
    
    * create: rota postagens/adicionar postagens
    
    * templates
    
    * fix: mongoose feature
    
    * create: select para adicionar postagem

--- routes/admin.js
+++ routes/admin.js
@@ -13,7 +13,16 @@ router.get("/posts", (req, res) => {
 });
 
 router.get("/categorias", (req, res) => {
-  res.render("admin/categorias");
+  Categoria.find()
+    .lean()
+    .sort({ date: "desc" })
+    .then((categ) => {
+      res.render("admin/categorias", { categorias: categ });
+    })
+    .catch((err) => {
+      req.flash("error_msg", "houve um erro ao listar as categorias");
+      res.redirect("/admin");
+    });
 });
 
 router.get("/categorias/add", (req, res) => {
@@ -60,6 +69,82 @@ router.post("/categorias/nova", (req, res) => {
         res.redirect("/admin");
       });
   }
+});
+
+router.get("/categorias/edit/:id", (req, res) => {
+  Categoria.findOne({ _id: req.params.id })
+    .lean()
+    .then((categoria) => {
+      res.render("admin/editcategoria", {
+        categoria: categoria,
+      });
+    })
+    .catch((err) => {
+      req.flash("error_msg", "essa categoria não existe");
+      res.redirect("/admin/categorias");
+    });
+});
+
+router.post("/categorias/edit", (req, res) => {
+  Categoria.findOne({ _id: req.body.id })
+    .then((categ) => {
+      categ.nome = req.body.nome;
+      categ.slug = req.body.slug;
+      categ
+        .save()
+        .then(() => {
+          req.flash("success_msg", "Categoria editada com sucesso!");
+          res.redirect("/admin/categorias");
+        })
+        .catch((err) => {
+          req.flash(
+            "error_msg",
+            "Houve um erro ao salvar a edição da categoria"
+          );
+          res.redirect("/admin/categorias");
+        });
+    })
+    .catch(() => {
+      req.flash("error_msg", "houve um erro ao editar a categoria");
+      res.redirect("/admin/categorias");
+    });
+});
+
+router.post("/categorias/deletar", (req, res) => {
+  Categoria.deleteOne({ _id: req.body.id })
+    .then(() => {
+      req.flash("success_msg", "Categoria deletada com sucesso");
+      res.redirect("/admin/categorias");
+    })
+    .catch((err) => {
+      req.flash(
+        "error_msg",
+        "Houve um erro ao tentar deletar a categoria " + err
+      );
+      res.redirect("/admin/categorias");
+    });
+});
+
+router.get("/postagens", (req, res) => {
+  res.render("admin/postagens");
+});
+
+router.get("/postagens/add", (req, res) => {
+  Categoria.find()
+    .lean()
+    .then((categorias) => {
+      res.render("admin/addpostagem", { categorias: categorias });
+    })
+    .catch((err) => {
+      req.flash("error_msg", "Houve um erro para carregar os usuarios");
+      res.redirect("/admin/postagens");
+    });
+});
+
+router.get("/testando", (req, res) => {
+  Categoria.find().then((respost) => {
+    res.send(respost);
+  });
 });
 
 module.exports = router;
